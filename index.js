2a49b3816c21cbdedd3dd69c27dc9338a4276aae
Adam <adamprankin@gmail.com>
2022-09-14 16:20:52 -0600

    start server

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const  app = express();
+http.createServer(app).listen(80);
 const path = require('path'	);
 
 const PORT = process.env.PORT || 5000;
 
-//set staic folder
+//set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => console.log('server is listening on port' + PORT));
